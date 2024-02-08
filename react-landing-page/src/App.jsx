3f33b83222bfd737b12cbbced0df9801997efcc5
Juan Sebastian Zapata Ocampo <1144041080@u.icesi.edu.co>
2024-02-07 19:43:50 -0500

    Creates react landing page app

---
+++ react-landing-page/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {Header} from "./components/Header/Header.jsx"
+export default function App(){
+    return(
+        <>
+        <Header />
+        <main>
+            <h1>Hello!</h1>
+        </main>
+        <footer>
+
+        </footer>
+
+        </>
+    )
+}
---
+++ react-landing-page/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+export function Header(){
+    /* ! Hola */
+    return(
+        <header>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png" width="100px" />
+        <nav>
+            <ul>
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
