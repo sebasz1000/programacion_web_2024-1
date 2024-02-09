eeab612ca19506850321178feba066301798d6ff
Juan Sebastian Zapata Ocampo <1144041080@u.icesi.edu.co>
2024-02-08 19:29:41 -0500

    Improves header styles in react-landing-page

--- react-landing-page/src/components/Header/Header.jsx
+++ react-landing-page/src/components/Header/Header.jsx
@@ -1,15 +1,42 @@
+import "./Header.css"
+
+const links = [
+    {
+        id: crypto.randomUUID(),
+        text: "Sobre Nosotros"
+    },
+    {
+        id: crypto.randomUUID(),
+        text: "Servicios"
+    },
+    {
+        id: crypto.randomUUID(),
+        text: "Contactenos"
+    }
+]
+console.log(links)
 export function Header(){
     /* ! Hola */
     return(
         <header>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png" width="100px" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png" 
+             width="100px" />
         <nav>
             <ul>
-                <li>Link 1</li>
-                <li>Link 2</li>
-                <li>Link 3</li>
+                {
+                    links.map(({ id, text}) => {
+                        return (
+                            <li key={id}>{text}</li>
+                        )
+                    })
+                }
             </ul>
         </nav>
+        <button id="hamburguer-btn">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png" 
+                alt="Hamburger button"
+                width="80px" />
+            </button>
     </header>
     )
 }
