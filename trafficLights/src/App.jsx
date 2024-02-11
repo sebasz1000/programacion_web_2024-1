96fabf74f77b8749270ce133ae7dcf107813d668
Juan Sebastian Zapata Ocampo <sebasz1000@gmail.com>
2024-02-11 18:39:30 -0500

    Utils in trafficLighs

--- trafficLights/src/App.jsx
+++ trafficLights/src/App.jsx
@@ -1,12 +1,17 @@
 export default function App(){
   return(
     <>
-      <main>
-        <header>
-          <h1>Traffic Lights</h1>
-          <p>React events and hooks</p>
-        </header>
-      </main>
+     <div className="app-bg">
+      <div className="container">
+      <header>
+            <h1>Traffic Lights</h1>
+            <p>React events and hooks</p>
+          </header>
+        <main>
+          <p>Lights</p>
+        </main>
+      </div>
+      </div>
     </>
   )
 }
